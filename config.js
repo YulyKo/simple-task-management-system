9fac22d636f5159439707ed1fb8b9186de35f448
YulyKo <yuliakotenko1.0@gmail.com>
2021-05-29 01:11:30 +0300

    set config for prod and add file to .gitignore

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
-  // ssl: { rejectUnauthorized: false },
+  // ssl: isProduction, // for local work
+  ssl: { rejectUnauthorized: false }, // for hosting
 })
 
 module.exports = {pool}
