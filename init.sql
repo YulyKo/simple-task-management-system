7a6acb16e97ad86db04e602fd82f10c873b57286
YulyKo <yuliakotenko1.0@gmail.com>
2021-05-28 23:53:09 +0300

    set new initing data of db

--- init.sql
+++ init.sql
@@ -1,5 +1,25 @@
-CREATE TABLE IF NOT EXISTS "books" (
-  "id" SERIAL PRIMARY KEY,
-  "author" VARCHAR(255) NOT NULL,
-  "title" VARCHAR(255) NOT NULL
+CREATE TABLE "users" (
+  "email" varchar(255) NOT NULL,
+  "password" varchar(255) NOT NULL,
+  "username" varchar(100) NOT NULL,
+  "confirmeAt" boolean
 );
+
+CREATE TABLE "tasks" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar(255) NOT NULL,
+  "description" varchar(1255) NOT NULL,
+  "isDone" boolean,
+  "priority" int NOT NULL,
+  "dueDate" date NOT NULL,
+  "isArcived" boolean
+);
+
+CREATE TABLE "user_taks" (
+  "userEmail" int NOT NULL,
+  "taskId" int NOT NULL
+);
+
+ALTER TABLE "user_taks" ADD FOREIGN KEY ("taskId") REFERENCES "tasks" ("id");
+
+ALTER TABLE "user_taks" ADD FOREIGN KEY ("userEmail") REFERENCES "users" ("email");
