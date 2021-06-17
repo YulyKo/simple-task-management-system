455677819f9f34c5f430f6942275201aab385419
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-17 16:22:19 +0300

    task/created method for insert into task to db

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -1,14 +1,28 @@
 const db = require('../models/index');
-const uuid = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
-  create(req, res) {},
+  create(req, res) {
+    return db.tasks
+      .create({
+        id: uuidv4(),
+        title: req.body.title,
+        description: req.body.description,
+        priority: req.body.priority,
+        dueDate: req.body.dueDate,
+        isDone: false,
+      })
+      .then((taks) => res.status(201).send(taks.id))
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  },
 
-  getAll(req, res) {},
+  // getAll(req, res) {},
 
-  getById(req, res) {},
+  // getById(req, res) {},
 
-  update(req, res) {},
+  // update(req, res) {},
 
-  delete(req, res) {},
+  // delete(req, res) {},
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
+const taskController = require('../controllers').task;
+
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'API start here here',
   }));
+
+  app.post('/api/tasks', taskController.create);
 };
