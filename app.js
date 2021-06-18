913dc087dd827645a5900cc3d27a3a9333f09f43
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-18 15:15:45 +0300

    main/added sequelize to app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const sequelize = require('sequelize');
 const app = express();
+
 app.use(logger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(sequelize);
 
 require('./src/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
