4cf44cadbacd79faa76c5f387e572cf487b58cc2
YulyKo <yuliakotenko1.0@gmail.com>
2021-06-20 02:37:58 +0300

    main/fixed cors

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const sequelize = require('sequelize');
-const cors = require('cors');
 const app = express();
 
+app.use(cors({
+  origin: '*',
+}));
 app.use(logger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(sequelize);
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
 
 require('./src/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const taskController = require('../controllers').task;
+const cors = require('cors');
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'API start here here',
   }));
 
-  app.post('/api/tasks', taskController.create);
+  app.post('/api/tasks', cors(), taskController.create);
   app.get('/api/tasks', taskController.getAll);
   app.get('/api/tasks/:taskId', taskController.getById);
   app.put('/api/tasks/:taskId', taskController.update);
